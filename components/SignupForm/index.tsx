7d5b25329dbb24cd3b6b3f72b14d111002918aae
Janagiramanm <janagiraman@netiapps.com>
2022-03-10 17:04:39 +0530

    sass installed

--- components/SignupForm/index.tsx
+++ components/SignupForm/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import styles from "./signup.module.scss"
 
 export default function SignupForm(){
 
     return(
         <div>
-            <form action="">
-                
-            </form>
+                <div>Signup Form</div>
+                <div>
+                    <div className={`${styles.hello}`}>
+                        <div className="container">
+                            <div className="row">
+                                <div className={`col-md-6`}><h3>Test</h3></div>
+                                <div className={`col-md-6`}><h3>Test</h3></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
-}
+} 
